bc1508316f43006e6f364a194db5ddcef09c9e0c
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-08-29 14:46:05 -0700

    Created Product model

---
+++ BookHeap.Models/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookHeap.Models;
+
+public class Product
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; }
+
+    [Required]
+    public string Description { get; set; }
+
+    [Required]
+    public string ISBN { get; set; }
+
+    [Required]
+    public string Author { get; set; }
+
+    [Required]
+    [Range(1, 10000)]
+    public double ListPrice { get; set; }
+
+    [Required]
+    [Range(1, 10000)]
+    public double Price { get; set; }
+
+    [Required]
+    [Range(1, 10000)]
+    public double Price50 { get; set; }
+
+    [Required]
+    [Range(1, 10000)]
+    public double Price100 { get; set; }
+
+    public string ImageUrl { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    [Required]
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+
+    [Required]
+    public int CoverTypeId { get; set; }
+    public CoverType CoverType { get; set; }
+}
