eb1bc0b5c7d54fb0240711c38c54043a3687caf8
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-09-02 18:53:15 -0700

    Created Product View Model; Installed ViewFeatures package

---
+++ BookHeap.Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookHeap.Models.ViewModels;
+
+public class ProductViewModel
+{
+    public Product product { get; set; }
+    public IEnumerable<SelectListItem> CategoryList { get; set; }
+    public IEnumerable<SelectListItem> CoverTypeList { get; set; }
+}
