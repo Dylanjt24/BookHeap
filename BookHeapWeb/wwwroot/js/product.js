8b02733a0b229e9fb42db5ca5593e184cbb4dda0
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-09-05 22:15:43 -0700

    Added product.js file to load Product datatable in index view

---
+++ BookHeapWeb/wwwroot/js/product.js
@@ -0,0 +1,19 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#dataTbl').DataTable({
+        "ajax": {
+            "url": "/Admin/Products/GetAll"
+        },
+        "columns": [
+            { "data": "title", "width": "15%" },
+            { "data": "isbn", "width": "15%" },
+            { "data": "price", "width": "15%" },
+            { "data": "author", "width": "15%" }
+        ]
+    });
+}
