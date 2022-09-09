6beda948bad85e32d14ce2087bc1cb29a289b51c
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-09-08 21:54:46 -0700

    Changed property display names in Product model

--- BookHeap.Models/Product.cs
+++ BookHeap.Models/Product.cs
@@ -27,18 +27,22 @@ public class Product
 
     [Required]
     [Range(1, 10000)]
+    [Display(Name = "List Price")]
     public double ListPrice { get; set; }
 
     [Required]
     [Range(1, 10000)]
+    [Display(Name = "Price for 1-49")]
     public double Price { get; set; }
 
     [Required]
     [Range(1, 10000)]
+    [Display(Name = "Price for 50-99")]
     public double Price50 { get; set; }
 
     [Required]
     [Range(1, 10000)]
+    [Display(Name = "Price for 100+")]
     public double Price100 { get; set; }
 
     [ValidateNever]
@@ -48,11 +52,13 @@ public class Product
     public DateTime UpdatedAt { get; set; } = DateTime.Now;
 
     [Required]
+    [Display(Name = "Category")]
     public int CategoryId { get; set; }
     [ValidateNever]
     public Category Category { get; set; }
 
     [Required]
+    [Display(Name = "Cover Type")]
     public int CoverTypeId { get; set; }
     [ValidateNever]
     public CoverType CoverType { get; set; }
