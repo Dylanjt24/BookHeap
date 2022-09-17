c796a2fb0a7e8259870d690693e131caf3ae674c
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-09-16 21:49:33 -0700

    Created company model and migrated table to db

--- BookHeap.DataAccess/Data/ApplicationDbContext.cs
+++ BookHeap.DataAccess/Data/ApplicationDbContext.cs
@@ -14,5 +14,6 @@ namespace BookHeap.DataAccess
         public DbSet<CoverType> CoverTypes { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Company> Companies { get; set; }
     }
 }
