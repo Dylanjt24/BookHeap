108e205128d3fa62312b1afc1a3640222cdf4b7b
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-09-16 18:47:02 -0700

    Added _LoginPartial to _Layout and changed register/login link styling; Temporarily removed RequireConfirmedAccount from DefaultItdentity service options

--- BookHeapWeb/Program.cs
+++ BookHeapWeb/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
