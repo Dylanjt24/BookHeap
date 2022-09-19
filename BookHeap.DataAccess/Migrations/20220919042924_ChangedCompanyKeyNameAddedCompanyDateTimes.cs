3363c7f3a78919284315e1ab696dbb3e2a0aefb2
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-09-18 21:32:03 -0700

    Changed Company key to CompanyId and added Company UpdatedAt/CreatedAt fields and migrated to db

---
+++ BookHeap.DataAccess/Migrations/20220919042924_ChangedCompanyKeyNameAddedCompanyDateTimes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookHeapWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedCompanyKeyNameAddedCompanyDateTimes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Companies",
+                newName: "CompanyId");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Companies",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Companies",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Companies");
+
+            migrationBuilder.RenameColumn(
+                name: "CompanyId",
+                table: "Companies",
+                newName: "Id");
+        }
+    }
+}
