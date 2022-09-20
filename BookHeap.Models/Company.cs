4f5cae08c080eb5107c0dbd2056bf1cda8a241bb
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-09-19 17:32:52 -0700

    Added display name tag helpers to company properties

--- BookHeap.Models/Company.cs
+++ BookHeap.Models/Company.cs
@@ -15,14 +15,17 @@ public class Company
     [Required]
     public string Name { get; set; }
 
+    [Display(Name = "Street Address")]
     public string? StreetAddress { get; set; }
 
     public string? City { get; set; }
 
     public string? State { get; set; }
 
+    [Display(Name = "Postal Code")]
     public string? PostalCode { get; set; }
 
+    [Display(Name = "Phone Number")]
     public string? PhoneNumber { get; set; }
 
     public DateTime CreatedAt { get; set; } = DateTime.Now;
