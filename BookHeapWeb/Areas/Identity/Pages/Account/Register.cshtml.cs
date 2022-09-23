5b036609450bc6612252c3df544c87a6155f745c
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-09-22 21:43:52 -0700

    Added logic in register post method to give user role on creation

--- BookHeapWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookHeapWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -170,6 +170,15 @@ namespace BookHeapWeb.Areas.Identity.Pages.Account
                 {
                     _logger.LogInformation("User created a new account with password.");
 
+                    if (Input.Role == null)
+                    {
+                        await _userManager.AddToRoleAsync(user, SD.Role_User_Indi);
+                    }
+                    else
+                    {
+                        await _userManager.AddToRoleAsync(user, Input.Role);
+                    }
+
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
