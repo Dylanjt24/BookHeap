72ca4fb43d99ee501ec4b1269945da79b0e139b8
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-09-24 22:29:12 -0700

    Created initial CartController with Index action method;

---
+++ BookHeapWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookHeapWeb.Areas.Customer.Controllers;
+
+[Area("Customer")]
+public class CartController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
