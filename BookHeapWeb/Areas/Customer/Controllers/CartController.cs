69e17f9992931b4f9a0f6750c56a26cca57015a4
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-09-25 15:13:31 -0700

    Added logic for getting all shopping carts for logged in user to Cart index action method

--- BookHeapWeb/Areas/Customer/Controllers/CartController.cs
+++ BookHeapWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookHeap.DataAccess.Repository.IRepository;
+using BookHeap.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BookHeapWeb.Areas.Customer.Controllers;
 
 [Area("Customer")]
+[Authorize]
 public class CartController : Controller
 {
+    private readonly IUnitOfWork _unitOfWork;
+    public ShoppingCartVM ShoppingCartVM { get; set; }
+    public CartController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
     public IActionResult Index()
     {
-        return View();
+        var claimsIdentity = (ClaimsIdentity)User.Identity;
+        var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+        ShoppingCartVM = new ShoppingCartVM()
+        {
+            // Get all shopping carts with the logged in user id and include Products
+            CartList = _unitOfWork.ShoppingCarts.GetAll(c => c.ApplicationUserId == claim.Value, "Product")
+        };
+        return View(ShoppingCartVM);
     }
 }
