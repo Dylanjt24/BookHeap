80bed69398fa1563f75acd3bbba347318e589094
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-09-25 15:12:34 -0700

    Added filter parameter to GetAll method

--- BookHeap.DataAccess/Repository/Repository.cs
+++ BookHeap.DataAccess/Repository/Repository.cs
@@ -24,9 +24,10 @@ namespace BookHeap.DataAccess.Repository
             dbSet.Add(entity);
         }
 
-        public IEnumerable<T> GetAll(string? includeProperties = null)
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
+            query = query.Where(filter);
             if (includeProperties != null)
                 foreach (var property in includeProperties.Split(',', StringSplitOptions.RemoveEmptyEntries))
                     query = query.Include(property);
