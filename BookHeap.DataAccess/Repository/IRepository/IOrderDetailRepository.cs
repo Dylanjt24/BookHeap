bad4c6dd7a3c14d75494badb49b6491204011d76
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-09-26 23:55:29 -0700

    Created OrderDetail repository

---
+++ BookHeap.DataAccess/Repository/IRepository/IOrderDetailRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookHeap.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookHeap.DataAccess.Repository.IRepository
+{
+    public interface IOrderDetailRepository : IRepository<OrderDetail>
+    {
+        void Update(OrderDetail orderDetail);
+    }
+}
---
+++ BookHeap.DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,24 @@
+﻿using BookHeap.DataAccess.Repository.IRepository;
+using BookHeap.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookHeap.DataAccess.Repository
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderDetailRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(OrderDetail orderDetail)
+        {
+            _db.OrderDetails.Update(orderDetail);
+        }
+    }
+}
