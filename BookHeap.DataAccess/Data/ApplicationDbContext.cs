2f496216dde91c041f31f0382d4be860864ac787
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-09-26 23:39:42 -0700

    Added OrderHeader and OrderDetail to dbcontext and pushed to db

--- BookHeap.DataAccess/Data/ApplicationDbContext.cs
+++ BookHeap.DataAccess/Data/ApplicationDbContext.cs
@@ -16,5 +16,7 @@ namespace BookHeap.DataAccess
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
     }
 }
