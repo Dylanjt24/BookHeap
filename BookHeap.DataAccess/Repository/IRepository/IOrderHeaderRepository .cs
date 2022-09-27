74298b0eeeed339eaec8f01e59e9f7e3bfa9ba3e
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-09-26 23:55:38 -0700

    Created OrderHeader repository

---
+++ BookHeap.DataAccess/Repository/IRepository/IOrderHeaderRepository .cs
@@ -0,0 +1,14 @@
+﻿using BookHeap.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookHeap.DataAccess.Repository.IRepository
+{
+    public interface IOrderHeaderRepository : IRepository<OrderHeader>
+    {
+        void UpdateStatus(int id, string status, string? paymentStatus = null);
+    }
+}
---
+++ BookHeap.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,31 @@
+﻿using BookHeap.DataAccess.Repository.IRepository;
+using BookHeap.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookHeap.DataAccess.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderHeaderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+        {
+            OrderHeader dbOrder = _db.OrderHeaders.FirstOrDefault(o => o.OrderHeaderId == id);
+            if (dbOrder != null)
+            {
+                dbOrder.OrderStatus = orderStatus;
+                dbOrder.UpdatedAt = DateTime.Now;
+                if (paymentStatus != null)
+                    dbOrder.PaymentStatus = paymentStatus;
+            }
+        }
+    }
+}
