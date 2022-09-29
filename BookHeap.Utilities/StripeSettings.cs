1a0bacd33c74ffdb02faa7dd570c9da64fa77d72
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-09-28 21:25:10 -0700

    Created StripeSettings class to bind Stripe API keys to

---
+++ BookHeap.Utilities/StripeSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookHeap.Utilities;
+
+public class StripeSettings
+{
+    public string SecretKey { get; set; }
+    public string PublishableKey { get; set; }
+}
