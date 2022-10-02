21d87d23314a4e4bbb52bdd3f424bf06f984759a
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-10-01 20:30:20 -0700

    Created initial OrdersController with GetAll API method

---
+++ BookHeapWeb/Areas/Admin/Controllers/OrdersController.cs
@@ -0,0 +1,29 @@
+﻿using BookHeap.DataAccess.Repository.IRepository;
+using BookHeap.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookHeapWeb.Areas.Admin.Controllers;
+
+public class OrdersController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+    public OrdersController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    #region API CALLS
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        // Get all OrderHeaders and include ApplicationUser
+        IEnumerable<OrderHeader> orderHeaders = _unitOfWork.OrderHeaders.GetAll(null, "ApplicationUser");
+        return Json(new { data = orderHeaders });
+    }
+    #endregion
+}
