8f9ee59ec9125256dea3e96250dc0f2490526d0e
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-10-01 23:08:29 -0700

    Added switch statement to filter orders based on passed status

--- BookHeapWeb/Areas/Admin/Controllers/OrdersController.cs
+++ BookHeapWeb/Areas/Admin/Controllers/OrdersController.cs
@@ -1,6 +1,8 @@
 ﻿using BookHeap.DataAccess.Repository.IRepository;
 using BookHeap.Models;
+using BookHeap.Utilities;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace BookHeapWeb.Areas.Admin.Controllers;
 
@@ -20,10 +22,32 @@ public class OrdersController : Controller
 
     #region API CALLS
     [HttpGet]
-    public IActionResult GetAll()
+    public IActionResult GetAll(string status)
     {
         // Get all OrderHeaders and include ApplicationUser
         IEnumerable<OrderHeader> orderHeaders = _unitOfWork.OrderHeaders.GetAll(null, "ApplicationUser");
+
+        switch (status)
+        {
+            case "processing":
+                orderHeaders = orderHeaders.Where(o => o.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                break;
+
+            case "pending":
+                orderHeaders = orderHeaders.Where(o => o.OrderStatus == SD.StatusPending);
+                break;
+
+            case "completed":
+                orderHeaders = orderHeaders.Where(o => o.OrderStatus == SD.StatusShipped);
+                break;
+
+            case "approved":
+                orderHeaders = orderHeaders.Where(o => o.OrderStatus == SD.StatusApproved);
+                break;
+
+            default:
+                break;
+        }
         return Json(new { data = orderHeaders });
     }
     #endregion
