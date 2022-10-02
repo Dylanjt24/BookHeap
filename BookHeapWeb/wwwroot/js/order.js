c28b6841e1f631b4365835d92343143c119ea1a3
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-10-01 20:52:16 -0700

    Created order.js for displaying data table in index view

---
+++ BookHeapWeb/wwwroot/js/order.js
@@ -0,0 +1,32 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#dataTbl').DataTable({
+        "ajax": {
+            "url": "/Admin/Orders/GetAll"
+        },
+        "columns": [
+            { "data": "orderHeaderId", "width": "5%" },
+            { "data": "name", "width": "25%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "applicationUser.email", "width": "25%" },
+            { "data": "orderStatus", "width": "15%" },
+            { "data": "orderTotal", "width": "10%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="w-75 btn-group" role="group">
+                            <a href="/Admin/Orders/Details?orderId=${data}" class="btn btn-primary mx-2 rounded-pill"><i class="bi bi-pencil-square"></i></a>
+                        </div>
+                        `
+                },
+                "width": "5%"
+            }
+        ]
+    });
+}
