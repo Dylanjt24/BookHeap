33940189e3a9f59b9d29fac5728d86c4c4cb5306
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-10-21 01:16:46 -0700

    Added SeedDatabase function and called it in the pipeine config

--- BookHeapWeb/Program.cs
+++ BookHeapWeb/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using BookHeap.Utilities;
 using Stripe;
+using BookHeap.DataAccess.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,7 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProvid
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultUI();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 builder.Services.AddSingleton<IEmailSender, EmailSender>();
 builder.Services.AddRazorPages();
 // Configure cookies to route to correct paths
@@ -60,6 +62,8 @@ app.UseStaticFiles();
 app.UseRouting();
 // Assign global Stripe API key
 StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
+// Seed database with roles and admin user when pipeline is configured
+SeedDatabase();
 app.UseAuthentication(); ;
 app.UseAuthorization();
 app.UseSession();
@@ -69,3 +73,12 @@ app.MapControllerRoute(
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
